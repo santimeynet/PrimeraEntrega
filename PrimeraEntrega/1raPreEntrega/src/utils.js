8c7fe4a7e01af861c96e5be76081822e7b94a937
SantiagoMeynet <santimeynet@gmail.com>
2023-12-05 19:37:14 -0300

    Nuevo desafio websockets+handlebars

---
+++ PrimeraEntrega/1raPreEntrega/src/utils.js
@@ -0,0 +1,7 @@
+import { fileURLToPath, URL } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default __dirname;
